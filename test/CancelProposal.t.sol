db54fbecd916e009af64d797d0ce52c13ddaa9fc
Alexandro T. Netto <alex.t.netto@gmail.com>
2024-04-10 02:26:15 -0300

    test: attacker successfully got proposer role

--- test/CancelProposal.t.sol
+++ test/CancelProposal.t.sol
@@ -10,8 +10,8 @@ import { IWallet } from "../src/interfaces/IWallet.sol";
 contract CounterTest is Test {
     IToken public token;
     IGovernor public governor;
-    ITimelock public timelock;
-    IWallet public daoWallet;
+    ITimelock public tokenTimelock;
+    IWallet public timelock;
     bytes32 public constant TIMELOCK_ADMIN_ROLE = keccak256("TIMELOCK_ADMIN_ROLE");
     bytes32 public constant PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
     bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
@@ -23,8 +23,8 @@ contract CounterTest is Test {
 
         token = IToken(0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72);
         governor = IGovernor(0x323A76393544d5ecca80cd6ef2A560C6a395b7E3);
-        timelock = ITimelock(0xd7A029Db2585553978190dB5E85eC724Aa4dF23f);
-        daoWallet = IWallet(payable(0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7));
+        tokenTimelock = ITimelock(0xd7A029Db2585553978190dB5E85eC724Aa4dF23f);
+        timelock = IWallet(payable(0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7));
 
         labelAddresses();
     }
@@ -33,7 +33,7 @@ contract CounterTest is Test {
         address tokenInGovernor = governor.token();
         assertEq(tokenInGovernor, address(token));
 
-        bool governorIsProposerInWallet = daoWallet.hasRole(PROPOSER_ROLE, address(governor));
+        bool governorIsProposerInWallet = timelock.hasRole(PROPOSER_ROLE, address(governor));
         assertTrue(governorIsProposerInWallet);
     }
 
@@ -48,22 +48,21 @@ contract CounterTest is Test {
         // Need to advance 1 block for delegation to be valid on governor
         vm.roll(block.number + 1);
 
-
-
         // Creating a proposal that gives a proposer role to
         address[] memory targets = new address[](1);
-        targets[0] = address(daoWallet);
+        targets[0] = address(timelock);
         uint256[] memory values = new uint256[](1);
         values[0] = 0;
         bytes[] memory calldatas = new bytes[](1);
-        calldatas[0] = abi.encodeCall(daoWallet.grantRole, (daoWallet.PROPOSER_ROLE(), voter));
-
+        calldatas[0] = abi.encodeCall(timelock.grantRole, (timelock.PROPOSER_ROLE(), voter));
+        string memory description = "";
+        bytes32 descriptionHash = keccak256(bytes(description));
         console.log(governor.proposalThreshold());
 
         // Governor //
         // Submit malicious proposal
         vm.prank(voter);
-        uint256 proposalId = governor.propose(targets, values, calldatas, "");
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
         assertEq(governor.state(proposalId), 0);
 
         // Proposal is ready to vote after 2 block because of the revert ERC20Votes: block not yet mined
@@ -79,12 +78,24 @@ contract CounterTest is Test {
         assertEq(governor.state(proposalId), 4);
 
         // Queue the proposal to be executed
-        //// Test if Operation status on timelock is Pending and investigate what state is returned on state()
+        governor.queue(targets, values, calldatas, descriptionHash);
+        assertEq(governor.state(proposalId), 5);
+
+        // Calculate proposalId in timelock
+        bytes32 proposalIdInTimelock =
+            timelock.hashOperationBatch(targets, values, calldatas, 0, descriptionHash);
+        assertTrue(timelock.isOperationPending(proposalIdInTimelock));
 
         // Wait the operation in the DAO wallet timelock to be Ready
+        vm.warp(block.timestamp + timelock.getMinDelay() + 1);
+        assertTrue(timelock.isOperationReady(proposalIdInTimelock));
 
-        // DAO Wallet (Timelock) //
+        // Execute proposal
+        governor.execute(targets, values, calldatas, descriptionHash);
+        assertTrue(timelock.isOperationDone(proposalIdInTimelock));
 
+
+        assertTrue(timelock.hasRole(PROPOSER_ROLE, voter));
         // To cancel the operation, the status needs to be pending and only the PROPOSAL_ROLE can call it
     }
 
@@ -93,7 +104,7 @@ contract CounterTest is Test {
         vm.label({ account: voter, newLabel: "VOTER" });
         vm.label({ account: address(governor), newLabel: "governor" });
         vm.label({ account: address(timelock), newLabel: "timelock" });
-        vm.label({ account: address(daoWallet), newLabel: "daoWallet" });
+        vm.label({ account: address(timelock), newLabel: "timelock" });
         vm.label({ account: address(token), newLabel: "token" });
     }
 }
