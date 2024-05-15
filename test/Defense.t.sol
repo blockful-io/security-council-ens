1a09609877eea1f31be39a1b0c96fcf6eaffa9e0
Alexandro T. Netto <alex.t.netto@gmail.com>
2024-05-15 14:34:04 -0300

    chore: simpler solution to give role on tests

--- test/Defense.t.sol
+++ test/Defense.t.sol
@@ -41,31 +41,45 @@ contract SecurityCouncilVeto_Test is Test {
     }
 
     function test_DefenseDAO() public {
-        // Submit security council proposal
+        // Give security council contract the role
+        vm.startPrank(address(timelock));
+        timelock.grantRole(timelock.PROPOSER_ROLE(), address(securityCouncilVeto));
+        vm.stopPrank();
 
+        // Check result
+        assertTrue(timelock.hasRole(PROPOSER_ROLE, address(securityCouncilVeto)));
+
+        // Start attack
         // Delegate from top token holder (binance, with 4m $ENS in this case)
         vm.prank(0x5a52E96BAcdaBb82fd05763E25335261B270Efcb);
-        token.delegate(voter);
+        token.delegate(attacker);
 
-        uint256 votingPower = token.getVotes(voter);
+        uint256 votingPower = token.getVotes(attacker);
         assertEq(votingPower, 4_126_912_192_000_000_000_000_000);
 
         // Need to advance 1 block for delegation to be valid on governor
         vm.roll(block.number + 1);
 
-        // Creating a proposal that gives a proposer role to
-        address[] memory targets = new address[](1);
+        // Creating a malicious proposal to takeover the timelock
+        address[] memory targets = new address[](3);
         targets[0] = address(timelock);
-        uint256[] memory values = new uint256[](1);
+        targets[1] = address(timelock);
+        targets[2] = attacker;
+        uint256[] memory values = new uint256[](3);
         values[0] = 0;
-        bytes[] memory calldatas = new bytes[](1);
-        calldatas[0] = abi.encodeCall(timelock.grantRole, (timelock.PROPOSER_ROLE(), address(securityCouncilVeto)));
+        values[1] = 0;
+        values[2] = 5_370_845_482_402_118_767_544;
+        bytes[] memory calldatas = new bytes[](3);
+        calldatas[0] = abi.encodeCall(timelock.grantRole, (timelock.PROPOSER_ROLE(), attacker));
+        calldatas[1] = abi.encodeCall(timelock.revokeRole, (timelock.PROPOSER_ROLE(), address(governor)));
+        calldatas[2] = bytes("");
 
         string memory description = "";
         bytes32 descriptionHash = keccak256(bytes(description));
 
         // Governor //
-        vm.prank(voter);
+        // Submit malicious proposal
+        vm.prank(attacker);
         uint256 proposalId = governor.propose(targets, values, calldatas, description);
         assertEq(governor.state(proposalId), 0);
 
@@ -74,7 +88,7 @@ contract SecurityCouncilVeto_Test is Test {
         assertEq(governor.state(proposalId), 1);
 
         // Vote for the proposal
-        vm.prank(voter);
+        vm.prank(attacker);
         governor.castVote(proposalId, 1);
 
         // Let the voting end
@@ -86,72 +100,7 @@ contract SecurityCouncilVeto_Test is Test {
         assertEq(governor.state(proposalId), 5);
 
         // Calculate proposalId in timelock
-        bytes32 proposalIdInTimelock = timelock.hashOperationBatch(targets, values, calldatas, 0, descriptionHash);
-        assertTrue(timelock.isOperationPending(proposalIdInTimelock));
-
-        // Wait the operation in the DAO wallet timelock to be Ready
-        vm.warp(block.timestamp + timelock.getMinDelay() + 1);
-        assertTrue(timelock.isOperationReady(proposalIdInTimelock));
-
-        // Execute proposal
-        governor.execute(targets, values, calldatas, descriptionHash);
-        assertTrue(timelock.isOperationDone(proposalIdInTimelock));
-
-        // Check result
-        assertTrue(timelock.hasRole(PROPOSER_ROLE, address(securityCouncilVeto)));
-
-        // Start attack
-        // Delegate from top token holder (binance, with 4m $ENS in this case)
-        vm.prank(0x5a52E96BAcdaBb82fd05763E25335261B270Efcb);
-        token.delegate(attacker);
-
-        uint256 votingPower2 = token.getVotes(attacker);
-        assertEq(votingPower2, 4_126_912_192_000_000_000_000_000);
-
-        // Need to advance 1 block for delegation to be valid on governor
-        vm.roll(block.number + 1);
-
-        // Creating a proposal that gives a proposer role to
-        address[] memory targets2 = new address[](3);
-        targets2[0] = address(timelock);
-        targets2[1] = address(timelock);
-        targets2[2] = attacker;
-        uint256[] memory values2 = new uint256[](3);
-        values2[0] = 0;
-        values2[1] = 0;
-        values2[2] = 5_370_845_482_402_118_767_544;
-        bytes[] memory calldatas2 = new bytes[](3);
-        calldatas2[0] = abi.encodeCall(timelock.grantRole, (timelock.PROPOSER_ROLE(), attacker));
-        calldatas2[1] = abi.encodeCall(timelock.revokeRole, (timelock.PROPOSER_ROLE(), address(governor)));
-        calldatas2[2] = bytes("");
-
-        string memory description2 = "";
-        bytes32 descriptionHash2 = keccak256(bytes(description2));
-
-        // Governor //
-        // Submit malicious proposal
-        vm.prank(attacker);
-        uint256 proposalId2 = governor.propose(targets2, values2, calldatas2, description2);
-        assertEq(governor.state(proposalId2), 0);
-
-        // Proposal is ready to vote after 2 block because of the revert ERC20Votes: block not yet mined
-        vm.roll(block.number + governor.votingDelay() + 1);
-        assertEq(governor.state(proposalId2), 1);
-
-        // Vote for the proposal
-        vm.prank(attacker);
-        governor.castVote(proposalId2, 1);
-
-        // Let the voting end
-        vm.roll(block.number + governor.votingPeriod());
-        assertEq(governor.state(proposalId2), 4);
-
-        // Queue the proposal to be executed
-        governor.queue(targets2, values2, calldatas2, descriptionHash2);
-        assertEq(governor.state(proposalId2), 5);
-
-        // Calculate proposalId in timelock
-        bytes32 proposalIdInTimelock2 = timelock.hashOperationBatch(targets2, values2, calldatas2, 0, descriptionHash2);
+        bytes32 proposalIdInTimelock2 = timelock.hashOperationBatch(targets, values, calldatas, 0, descriptionHash);
         assertTrue(timelock.isOperationPending(proposalIdInTimelock2));
 
         securityCouncilVeto.veto(proposalIdInTimelock2);
@@ -162,7 +111,7 @@ contract SecurityCouncilVeto_Test is Test {
 
         // Execute proposal
         vm.expectRevert("TimelockController: operation is not ready");
-        governor.execute(targets2, values2, calldatas2, descriptionHash2);
+        governor.execute(targets, values, calldatas, descriptionHash);
         assertFalse(timelock.isOperationDone(proposalIdInTimelock2));
 
         // Check result
